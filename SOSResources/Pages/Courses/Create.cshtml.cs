c0523e0f7eebc769fb2f5c0d67db6c0481a07464
WrightSE <104210666+WrightSE@users.noreply.github.com>
2023-11-01 22:03:09 -0500

    Scaffolded courses

---
+++ SOSResources/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SOSResources.Data;
+using SOSResources.Models;
+
+namespace SOSResources.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SOSResources.Data.SchoolContext _context;
+
+        public CreateModel(SOSResources.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
