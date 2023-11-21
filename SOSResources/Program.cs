725be306cabe47bf2b96277aad13ef944119da3c
Zach B <zach@bernheimer.us>
2023-11-20 19:43:32 -0600

    added ensureCreated call to program.cs

--- SOSResources/Program.cs
+++ SOSResources/Program.cs
@@ -30,6 +30,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<SOSContext>();
+    context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
