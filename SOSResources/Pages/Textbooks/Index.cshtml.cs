2fdbdf206189025fa2a88b693368e40c4889bce6
Zach B <zach@bernheimer.us>
2023-12-08 18:52:18 -0600

    Added search and filter by title and author to textbook index

--- SOSResources/Pages/Textbooks/Index.cshtml.cs
+++ SOSResources/Pages/Textbooks/Index.cshtml.cs
@@ -19,13 +19,53 @@ namespace SOSResources.Pages.Textbooks
             _context = context;
         }
 
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string TitleFilter { get; set; }
+        public string AuthorFilter { get; set; }
+        public string CurrentSort { get; set; }
+
         public IList<Textbook> Textbook { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string titleSearchString, string authorSearchString)
         {
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = sortOrder == "Author" ? "author_desc" : "Author";
+            TitleFilter = titleSearchString;
+            AuthorFilter = authorSearchString;
+
+            IQueryable<Textbook> TextbookIQ = from tb in _context.Textbooks
+                                            select tb;
+
+            if (!String.IsNullOrEmpty(authorSearchString) && !String.IsNullOrWhiteSpace(titleSearchString))
+            {
+                TextbookIQ = TextbookIQ.Where(tb => tb.Author.Contains(authorSearchString)
+                                && tb.Title.Contains(titleSearchString));
+            } else if (!String.IsNullOrWhiteSpace(titleSearchString)){
+                TextbookIQ = TextbookIQ.Where(tb => tb.Title.Contains(titleSearchString));
+            } else if (!String.IsNullOrWhiteSpace(authorSearchString)){
+                TextbookIQ = TextbookIQ.Where(tb => tb.Author.Contains(authorSearchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    TextbookIQ = TextbookIQ.OrderByDescending(tb => tb.Title);
+                    break;
+                case "Author":
+                    TextbookIQ = TextbookIQ.OrderBy(tb => tb.Author);
+                    break;
+                case "author_desc":
+                    TextbookIQ = TextbookIQ.OrderByDescending(tb => tb.Author);
+                    break;
+                default:
+                    TextbookIQ = TextbookIQ.OrderBy(tb => tb.Title);
+                    break;
+            }
+
             if (_context.Textbooks != null)
             {
-                Textbook = await _context.Textbooks.Include(t => t.Copies).ToListAsync();
+                Textbook = await TextbookIQ.Include(t => t.Copies).ToListAsync();
             }
         }
     }
