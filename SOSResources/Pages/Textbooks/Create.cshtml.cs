4eafe2d2e9ec625233425f82735b29fdd9726a24
Zach B <zach@bernheimer.us>
2023-12-09 00:41:17 -0600

    updated input handling for create and edit textbooks

--- SOSResources/Pages/Textbooks/Create.cshtml.cs
+++ SOSResources/Pages/Textbooks/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Tokens;
 using SOSResources.Data;
 using SOSResources.Models;
 
@@ -28,16 +29,41 @@ namespace SOSResources.Pages.Textbooks
         public Textbook Textbook { get; set; }
         
 
+        public int Copies { get; set; }
+
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int copies)
         {
           if (!ModelState.IsValid)
             {
                 return Page();
             }
-            _context.Textbooks.Add(Textbook);
+
+            //Check if the textbook already exists and only create it if not.
+            var matches = _context.Textbooks.Where(t => t.Title.ToUpper().Equals(Textbook.Title.ToUpper()) && t.Author.ToUpper().Equals(Textbook.Author.ToUpper()));
+            Textbook tb = null;
+            if (!matches.IsNullOrEmpty()){
+                if (Textbook.Edition.IsNullOrEmpty()){
+                    var editionMatch = matches.Where(t => String.IsNullOrWhiteSpace(t.Edition));
+                    if (!editionMatch.IsNullOrEmpty()){
+                        tb = editionMatch.ToArray()[0];
+                    }
+                } else {
+                    var editionMatch = matches.Where(t => t.Edition.Equals(Textbook.Edition));
+                    if (!editionMatch.IsNullOrEmpty()){
+                        tb = editionMatch.ToArray()[0];
+                    }
+                }
+            }
+
+            if (tb == null){
+                _context.Textbooks.Add(Textbook);
+            } else {
+                Textbook = tb;
+            }
+
             
-            int.TryParse(Request.Form["copies"].ToString(), out int copies);
+            //int.TryParse(Request.Form["copies"].ToString(), out int copies);
 
             for (int i = 0; i < copies; i++) {
                 Copy c = new Copy{
