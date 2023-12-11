adb771958d1a7551df817d3bbe3bb8fcec30b3a3
WrightSE <104210666+WrightSE@users.noreply.github.com>
2023-12-10 20:34:27 -0600

    inital migrations

--- Data/Migrations/20231211023407_InitialMigration.Designer.cs
+++ Data/Migrations/20231211023407_InitialMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SOS_Resources.Data;
 
@@ -10,9 +11,11 @@ using SOS_Resources.Data;
 namespace SOSResources.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231211023407_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
