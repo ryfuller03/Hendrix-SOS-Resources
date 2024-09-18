faccf740eb1c3f413eb0f82b87c0b1ffe39aaf65
Ryan Fuller <fullerra@hendrix.edu>
2024-09-17 19:02:09 -0500

    Added animations, rearranged page

--- SOS_Resources/wwwroot/js/site.js
+++ SOS_Resources/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((element, index) => {
+        element.style.animationDelay = `${index * 0.1}s`;
+        
+    });
+});
