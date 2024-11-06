178509c7b7a1ddb1c7fb10ad8e180d4844eef91b
keyszl <keyszl@hendrix.edu>
2024-11-06 15:01:56 -0600

    fixing the microsoft login with project rework

--- Program.cs
+++ Program.cs
@@ -1,14 +1,44 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using HendrixSOSResources.Data;
-var builder = WebApplication.CreateBuilder(args);
+
+// <ms_docref_import_types>
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+// </ms_docref_import_types>
+
+// <ms_docref_add_msal>
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+IEnumerable<string>? initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ');
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SOSContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SOSContext") ?? throw new InvalidOperationException("Connection string 'SOSContext' not found.")));
 
-var app = builder.Build();
+
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+        .AddDownstreamApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+        .AddInMemoryTokenCaches();
+// </ms_docref_add_msal>
+
+
+// <ms_docref_add_default_controller_for_sign-in-out>
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+                      .RequireAuthenticatedUser()
+                      .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    }).AddMicrosoftIdentityUI();
+// </ms_docref_add_default_controller_for_sign-in-out>
+
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,13 +48,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+// </ms_docref_enable_authz_capabilities>
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
