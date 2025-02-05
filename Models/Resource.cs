e615c13bfe5d82b90631d659d7c7cdd23bdf95d0
elliotallen1 <allened@hendrix.edu>
2025-02-05 00:07:57 -0600

    Updated Resource model
    Type is now a ResourceType enum. Removed Notes

--- Models/Resource.cs
+++ Models/Resource.cs
@@ -5,24 +5,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SOSResources.Models
 {
+    public enum ResourceType
+    {
+        FirstAidSupplies,
+        OverTheCounterMedication,
+        HygieneSupplies,
+        PersonalCareSupplies,
+        Textbook
+    }
+
     public class Resource
     {
         public int ID { get; set; }
+
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(30)]
-        public string Type { get; set; }
+        public ResourceType Type { get; set; }
 
         [StringLength(250)]
         public string Description { get; set; }
 
         public int Quantity { get; set; }
-
-        public Textbook? TextbookItem { get; set; }
-
-        public string? Notes { get; set; }
     }
 }
