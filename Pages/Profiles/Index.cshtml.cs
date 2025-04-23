36598b75b17706515ee304f909109ff4a21932d1
elliotallen1 <allened@hendrix.edu>
2025-04-23 05:53:49 -0500

    Forgot one

--- Pages/Profiles/Index.cshtml.cs
+++ Pages/Profiles/Index.cshtml.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HendrixSOSResources.Data;
 using SOSResources.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HendrixSOSResources.Pages.Profiles
 {
+    [Authorize(Policy = "RequireAdministratorRole")]
     public class IndexModel : PageModel
     {
         private readonly HendrixSOSResources.Data.SOSContext _context;
